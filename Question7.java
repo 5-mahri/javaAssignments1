c40f15ccc4f1c97b143f2e9f225c784ead17b6e7
5-mahri <71532978+5-mahri@users.noreply.github.com>
2020-09-20 20:44:46 +0200

    Add files via upload

---
+++ Question7.java
@@ -0,0 +1,16 @@
+package day_2;
+
+public class Question7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println(" /\\ ");
+		System.out.println("/__\\");
+		System.out.println();
+		System.out.println("I am from \"Germany\"");
+		System.out.println("\n"+"How is your day going on,"+" "+"\'good\' or \'bad\'?");
+
+	}
+
+}
